2fc6ae982018169612554ba97bb95d041f295d17
alaidine <aa.alimlinde@gmail.com>
2025-11-19 10:23:47 +0400

    The client has a moving ball

--- src/client.cpp
+++ src/client.cpp
@@ -1,66 +1,34 @@
-/*******************************************************************************************
-*
-*   raylib [core] example - input keys
-*
-*   Example complexity rating: [★☆☆☆] 1/4
-*
-*   Example originally created with raylib 1.0, last time updated with raylib 1.0
-*
-*   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
-*   BSD-like license that allows static linking with closed source software
-*
-*   Copyright (c) 2014-2025 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
-
 #include "raylib.h"
 
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
-int main(void)
+int main(int argc, char **argv)
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
     const int screenWidth = 800;
     const int screenHeight = 450;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - input keys");
+    InitWindow(screenWidth, screenHeight, "r-type");
 
-    Vector2 ballPosition = { (float)screenWidth/2, (float)screenHeight/2 };
+    Vector2 boxPosition = { (float)screenWidth/2, (float)screenHeight/2 };
+    Vector2 boxDimension = { (float)50, (float)50 };
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
+    SetTargetFPS(60);
 
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+    while (!WindowShouldClose())
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        if (IsKeyDown(KEY_RIGHT)) ballPosition.x += 2.0f;
-        if (IsKeyDown(KEY_LEFT)) ballPosition.x -= 2.0f;
-        if (IsKeyDown(KEY_UP)) ballPosition.y -= 2.0f;
-        if (IsKeyDown(KEY_DOWN)) ballPosition.y += 2.0f;
-        //----------------------------------------------------------------------------------
+        if (IsKeyDown(KEY_RIGHT)) boxPosition.x += 2.0f;
+        if (IsKeyDown(KEY_LEFT)) boxPosition.x -= 2.0f;
+        if (IsKeyDown(KEY_UP)) boxPosition.y -= 2.0f;
+        if (IsKeyDown(KEY_DOWN)) boxPosition.y += 2.0f;
 
-        // Draw
-        //----------------------------------------------------------------------------------
         BeginDrawing();
 
-            ClearBackground(RAYWHITE);
-
-            DrawText("move the ball with arrow keys", 10, 10, 20, DARKGRAY);
-
-            DrawCircleV(ballPosition, 50, MAROON);
+        ClearBackground(RAYWHITE);
+        DrawText("move the box with arrow keys", 10, 10, 20, DARKGRAY);
+        DrawRectangleV(boxPosition, boxDimension, MAROON);
 
         EndDrawing();
-        //----------------------------------------------------------------------------------
     }
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
+    CloseWindow();
 
     return 0;
 }
