305a043342e91b2c0740aa13d59ac5ccca6528fa
alaidine <aa.alimlinde@gmail.com>
2026-01-20 16:16:33 +0400

    Separate the engine cmake from the tests cmake

---
+++ src/tests/conanfile.py
@@ -0,0 +1,21 @@
+from conan import ConanFile
+from conan.tools.cmake import cmake_layout
+
+
+class CompressorRecipe(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeToolchain", "CMakeDeps"
+
+    def requirements(self):
+        self.requires("box2d/3.1.1")
+        self.requires("asio/1.36.0")
+        self.requires("spdlog/1.16.0")
+        self.requires("glm/1.0.1")
+        self.requires("raylib/5.5")
+
+    def configure(self):
+        self.options["raylib"].shared = True
+        self.options["box2d"].shared = True
+    
+    def layout(self):
+        cmake_layout(self)
